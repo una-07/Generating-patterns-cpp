d5c60443a786e2908be4817f014f8aae9a7881eb
una-07 <esirkepulpan8@gmail.com>
2026-02-22 11:39:24 +0500

    Create HtmlReportBuilder.h

---
+++ builder-report/HtmlReportBuilder.h
@@ -0,0 +1,12 @@
+#pragma once
+#include "IReportBuilder.h"
+
+class HtmlReportBuilder : public IReportBuilder {
+private:
+    Report report;
+public:
+    void SetHeader(const std::string& h) override;
+    void SetContent(const std::string& c) override;
+    void SetFooter(const std::string& f) override;
+    Report GetReport() override;
+};
